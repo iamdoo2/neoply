319653990967d2ba97da3b5823b001d1c5a4414a
EC2 Default User <ec2-user@ip-10-197-28-41.us-west-1.compute.internal>
2014-03-03 05:08:10 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Neoply::Application.config.secret_key_base = 'b56999506db5e97b3126636703c88c707f53778a7af4a0c01064141867ee3a83135b47ff664327b4bd543d624627d26ec55870d79a77f5640135249e1f906c41'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Neoply::Application.config.session_store :cookie_store, key: '_neoply_session'
