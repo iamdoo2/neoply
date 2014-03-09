1209a89c29b3813a9306a3011f0ead25fcd9fd8b
Duhee Lee <iamdoo2@gmail.com>
2014-03-09 19:15:52 +0900

    4 companies are added.

---
+++ public/js/neoply.js
@@ -0,0 +1,25 @@
+var companies = 
+  [
+    ["current", "likelion", "라이크라이언", 
+    "온라인 프로그래밍 교육 스타트업입니다.<br>서울대학교 동아리 '멋쟁이 사자처럼'에 뿌리를 두고 있으며, KISA와 공동으로 코드라이언(<a href='http://codelion.net' target='_blank' style='color:white;'>http://codelion.net</a>)을 진행하고 있습니다."],
+    ["current", "carffeine", "카페인", 
+    "온라인 프로그래밍 교육 스타트업입니다.<br>서울대학교 동아리 '멋쟁이 사자처럼'에 뿌리를 두고 있으며, KISA와 공동으로 코드라이언(<a href='http://codelion.net' target='_blank' style='color:white;'>http://codelion.net</a>)을 진행하고 있습니다."]
+  ]
+
+$(function(){
+/*
+          <li class="current">
+            <div class="isotope-alt-image">
+              <img src="img/likelion.png" alt="image" />
+              <div>
+                <h4 style="margin-bottom:0px;">라이크라이언</h4>
+                <p>온라인 프로그래밍 교육 스타트업입니다.<br>
+                서울대동아리 '멋쟁이 사자처럼'에 뿌리를 두고 있으며, KISA와 공동으로 코드라이언(<a href="http://codelion.net" target="_blank" style="color:white;">http://codelion.net</a>)을 진행하고 있습니다.</p>
+                </p>
+              </div>
+            </a>
+          </li>
+
+*/
+});
+
